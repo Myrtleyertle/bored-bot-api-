b64fbc3264bec52deb0f5c7b2df2c7b60c000df8
Myrtleyertle <89166514+Myrtleyertle@users.noreply.github.com>
2021-11-05 13:43:00 -0500

    update
    removed border

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ document.getElementById("btn").addEventListener("click", function() {
     fetch('https://apis.scrimba.com/bored/api/activity')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('activity').textContent = `🤖${data.activity}🤖`
+            document.getElementById('activity').textContent = `🤖${data.activity}🤖`;
         })
 });
